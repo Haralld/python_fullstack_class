9e48477f8b1b4ee6b72d3ceac94463e6de4c216d
Haralld <71228924+Haralld@users.noreply.github.com>
2023-11-26 16:22:31 +0100

    Add files via upload

---
+++ module_2/lesson_9.2/solution_1.py
@@ -0,0 +1,23 @@
+input_items_price = input("Введите цены и скидку: ")
+price_list = [int(x) for x in input_items_price.split(', ')]
+
+def calculate_discount(prices):
+    price_list = prices[:-1]
+    sum_price = sum(price_list)
+    discount = sum_price * (prices[-1] / 100)
+    return discount
+
+total_discount = calculate_discount(price_list)
+
+print(f"Сумма скидки: {total_discount}")
+
+"""
+Задача 1: Подсчет скидки
+
+Описание:
+
+Роман хочет узнать, какую сумму он сэкономит покупателям, если предложит скидку на товары в его магазине. 
+Напишите функцию `calculate_discount`, которая принимает на вход список цен товаров и процент скидки, 
+при вводе скидка будет последним числом.
+ Функция должна возвращать сумму скидки на все товары.
+"""

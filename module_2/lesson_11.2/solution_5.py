b1bc7fc1429fdddbfe30d95d7a498cd6d5bb564b
Haralld <71228924+Haralld@users.noreply.github.com>
2023-12-05 21:36:43 +0100

    Update solution_5.py

--- module_2/lesson_11.2/solution_5.py
+++ module_2/lesson_11.2/solution_5.py
@@ -2,12 +2,7 @@ month_len_1 = 30
 month_len_2 = 31
 
 def sales_schedule_with_range(month_len):
-    sales_days = []
-    
-    for day in range(3, month_len + 1, 3):
-        sales_days.append(day)
-
-    return sales_days
+    return list(range(3, month_len + 1, 3))
 
 month_1 = sales_schedule_with_range(month_len_1)
 month_2 = sales_schedule_with_range(month_len_2)

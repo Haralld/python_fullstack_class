c70630d6fad2874e92f12e3060e7ededdb1e903c
Haralld <71228924+Haralld@users.noreply.github.com>
2024-01-03 15:00:07 +0100

    Update solution_5.py

--- module_2/lesson_14/solution_5.py
+++ module_2/lesson_14/solution_5.py
@@ -1,11 +1,11 @@
 from collections import Counter
 
-def inventory_counter(items, types):
+def inventory_counter(items):
     type_counts = Counter(map(lambda item: item[1], items))
     return dict(type_counts)
 
 # Пример использования
 items_list = [("item1", "electronics"), ("item2", "clothing"), ("item3", "electronics"), ("item4", "clothing"), ("item5", "books")]
 
-result = inventory_counter(items_list, types=["electronics", "clothing", "books"])
+result = inventory_counter(items_list)
 print(result)
